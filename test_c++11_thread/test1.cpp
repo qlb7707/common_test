6f00b45ea00b14b75276c90486df67c4fb330065
qlb7707 <770759450@qq.com>
2016-07-09 17:01:51 +0800

    add test c++11 thread

---
+++ test_c++11_thread/test1.cpp
@@ -0,0 +1,38 @@
+/*================================================================
+*   Copyright (C) 2016 All rights reserved.
+*   
+*   filename     :test.cpp
+*   author       :qinlibin
+*   create date  :2016/07/09
+*   mail         :qin_libin@foxmail.com
+*
+================================================================*/
+//#include "test.h"
+
+#include<atomic>
+#include<thread>
+#include<iostream>
+#include<mutex>
+using namespace std;
+
+long long total = 0;
+mutex mtx;
+void func(int)
+{
+    for(long long i = 0; i < 100000LL; i++)
+    {
+        mtx.lock();
+        total+=i;
+        mtx.unlock();
+    }
+}
+
+int main()
+{
+    thread t1(func,0);
+    thread t2(func,0);
+    t1.join();
+    t2.join();
+    cout<<total<<endl;
+    return 0;
+}
