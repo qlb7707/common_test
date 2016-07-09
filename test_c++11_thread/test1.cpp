61a2f90ca600b4835bdcc75409a22c9d32c392aa
qlb7707 <770759450@qq.com>
2016-07-09 17:11:39 +0800

    modify test c++ thread to support mutex and atomic

--- test_c++11_thread/test1.cpp
+++ test_c++11_thread/test1.cpp
@@ -13,17 +13,27 @@
 #include<thread>
 #include<iostream>
 #include<mutex>
+#define _USE_MUTEX_
 using namespace std;
-
+#ifdef _USE_MUTEX_
 long long total = 0;
 mutex mtx;
+#else
+atomic_llong total {0};
+#endif
+
+
 void func(int)
 {
     for(long long i = 0; i < 100000LL; i++)
     {
+#ifdef _USE_MUTEX_
         mtx.lock();
+#endif
         total+=i;
+#ifdef _USE_MUTEX_
         mtx.unlock();
+#endif
     }
 }
 
